0bc99712cdebf8561b5838c5d822ff22d1a59b5c
Zeeshan Ali Khan <zeeshanak@gnome.org>
2022-11-04 01:01:19 +0100

    tests: Ensure socket file is deleted even if failures happen

--- tests/fdo.rs
+++ tests/fdo.rs
@@ -1,7 +1,11 @@
-use std::{iter::repeat_with, path::PathBuf};
+use std::{
+    iter::repeat_with,
+    path::{Path, PathBuf},
+};
 
+use anyhow::ensure;
 use dbuz::bus::Bus;
-use tokio::select;
+use tokio::{select, sync::oneshot::Sender};
 use tracing::instrument;
 use tracing_subscriber::{util::SubscriberInitExt, EnvFilter, FmtSubscriber};
 use zbus::{
@@ -36,70 +40,67 @@ async fn name_ownership_changes() {
         bus
     });
 
+    let ret = name_ownership_changes_client(&*path, tx).await;
+    let bus = handle.await.unwrap();
+    bus.cleanup().await.unwrap();
+    ret.unwrap();
+}
+
+#[instrument]
+async fn name_ownership_changes_client(path: &Path, tx: Sender<()>) -> anyhow::Result<()> {
     let socket_addr = format!("unix:path={}", path.display());
-    let conn = ConnectionBuilder::address(&*socket_addr)
-        .unwrap()
-        .build()
-        .await
-        .unwrap();
+    let conn = ConnectionBuilder::address(&*socket_addr)?.build().await?;
     let dbus_proxy = DBusProxy::builder(&conn)
         .cache_properties(CacheProperties::No)
         .build()
-        .await
-        .unwrap();
-    let name: WellKnownName = "org.blah".try_into().unwrap();
+        .await?;
+    let name: WellKnownName = "org.blah".try_into()?;
 
     // This should work.
     let ret = dbus_proxy
         .request_name(name.clone(), RequestNameFlags::AllowReplacement.into())
-        .await
-        .unwrap();
-    assert_eq!(ret, RequestNameReply::PrimaryOwner);
+        .await?;
+    ensure!(
+        ret == RequestNameReply::PrimaryOwner,
+        "expected to become primary owner"
+    );
 
     // This shouldn't and we should be told we already own the name.
     let ret = dbus_proxy
         .request_name(name.clone(), RequestNameFlags::AllowReplacement.into())
-        .await
-        .unwrap();
-    assert_eq!(ret, RequestNameReply::AlreadyOwner);
+        .await?;
+    ensure!(
+        ret == RequestNameReply::AlreadyOwner,
+        "expected to be already primary owner"
+    );
 
     // Now we try with another connection and we should be queued.
-    let conn2 = ConnectionBuilder::address(&*socket_addr)
-        .unwrap()
-        .build()
-        .await
-        .unwrap();
+    let conn2 = ConnectionBuilder::address(&*socket_addr)?.build().await?;
     let dbus_proxy2 = DBusProxy::builder(&conn2)
         .cache_properties(CacheProperties::No)
         .build()
-        .await
-        .unwrap();
+        .await?;
     let ret = dbus_proxy2
         .request_name(name.clone(), Default::default())
-        .await
-        .unwrap();
+        .await?;
 
     // Check that first client is the primary owner before it releases the name.
-    assert_eq!(ret, RequestNameReply::InQueue);
-    let owner = dbus_proxy
-        .get_name_owner(name.clone().into())
-        .await
-        .unwrap();
-    assert_eq!(owner, *conn.unique_name().unwrap());
+    ensure!(ret == RequestNameReply::InQueue, "expected to be in queue");
+    let owner = dbus_proxy.get_name_owner(name.clone().into()).await?;
+    ensure!(owner == *conn.unique_name().unwrap(), "unexpected owner");
 
     // Now the first client releases name.
-    let ret = dbus_proxy.release_name(name.clone()).await.unwrap();
-    assert_eq!(ret, ReleaseNameReply::Released);
+    let ret = dbus_proxy.release_name(name.clone()).await?;
+    ensure!(
+        ret == ReleaseNameReply::Released,
+        "expected name to be released"
+    );
 
     // Now the second client should be the primary owner.
-    let owner = dbus_proxy
-        .get_name_owner(name.clone().into())
-        .await
-        .unwrap();
-    assert_eq!(owner, *conn2.unique_name().unwrap());
+    let owner = dbus_proxy.get_name_owner(name.clone().into()).await?;
+    ensure!(owner == *conn2.unique_name().unwrap(), "unexpected owner");
 
     tx.send(()).unwrap();
 
-    let bus = handle.await.unwrap();
-    bus.cleanup().await.unwrap();
+    Ok(())
 }
