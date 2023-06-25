c34894c748172cc9af2667f6d91894f44ac60485
Zeeshan Ali Khan <zeeshan_ali.khan@mercedes-benz.com>
2023-06-25 15:39:42 +0200

    🥚 Add easter egg method

--- src/fdo.rs
+++ src/fdo.rs
@@ -331,6 +331,14 @@ impl DBus {
         ))
     }
 
+    /// Easter egg method.
+    fn dune(&self) -> &str {
+        "I must not fear. Fear is the mind-killer. Fear is the little-death that brings total \
+        obliteration. I will face my fear. I will permit it to pass over me and through me. And \
+        when it has gone past I will turn the inner eye to see its path. Where the fear has gone \
+        there will be nothing. Only **I** will remain!"
+    }
+
     //
     // Propertries
     //
