babeb92c0dfd78db3e923d330a765c21fb2e195c
Ron Waldon-Howe <jokeyrhyme@jokeyrhy.me>
2024-11-30 11:58:28 +1100

    🔧 Read XML into Vec<Element>`, resolving `<include>` and `<includedir>`
    This is inspired by @elmarco 's work over in #23
    
    Importantly, by starting with this intermediate representation,
    we can preserve the intended order of elements

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,10 +1,23 @@
 use serde::Deserialize;
 
 pub mod limits;
+mod xml;
 
 #[derive(Clone, Debug, Deserialize, PartialEq)]
 #[serde(rename_all = "lowercase")]
 pub enum BusType {
     Session,
     System,
+}
+
+#[derive(Clone, Debug, Default, Deserialize, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub enum MessageType {
+    #[default]
+    #[serde(rename = "*")]
+    Any,
+    MethodCall,
+    MethodReturn,
+    Signal,
+    Error,
 }
