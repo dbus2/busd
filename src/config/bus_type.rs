655afecd8f3507ee99ea62bb9f4286050cc5e4da
Ron Waldon-Howe <jokeyrhyme@jokeyrhy.me>
2024-12-14 16:43:59 +1100

    🏷️ Implement `TryFrom<BusType>` for `Address`
    We'll use this later when we need the default bus address for each type.

--- src/config/bus_type.rs
+++ src/config/bus_type.rs
@@ -1,6 +1,43 @@
+#[cfg(unix)]
+use std::{env, path::PathBuf};
+
+use anyhow::{Error, Result};
+use zbus::Address;
+
 #[derive(Clone, Copy, Debug, Default, PartialEq)]
 pub enum BusType {
     #[default]
     Session,
     System,
+}
+
+impl TryFrom<BusType> for Address {
+    type Error = Error;
+    #[cfg(unix)]
+    fn try_from(value: BusType) -> Result<Self> {
+        if value == BusType::System {
+            return Address::try_from("unix:path=/run/dbus/system_bus_socket").map_err(Error::msg);
+        }
+
+        // BusType::Session
+        Address::try_from(format!("unix:tmpdir={}", default_session_dir().display()).as_str())
+            .map_err(Error::msg)
+    }
+
+    #[cfg(not(unix))]
+    fn try_from(_value: BusType) -> Result<Self> {
+        Address::try_from("tcp:host=127.0.0.1,port=4242").map_err(Error::msg)
+    }
+}
+
+#[cfg(unix)]
+fn default_session_dir() -> PathBuf {
+    env::var("XDG_RUNTIME_DIR")
+        .map(|s| PathBuf::from(s).to_path_buf())
+        .ok()
+        .unwrap_or_else(|| {
+            PathBuf::from("/run")
+                .join("user")
+                .join(format!("{}", nix::unistd::Uid::current()))
+        })
 }
